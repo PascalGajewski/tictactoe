5068d1b0c40c71ec65d064e64af98d3db6309a45
PascalGajewski <pascal.gajewski77@gmail.com>
2023-02-08 18:47:30 +0100

    added drawing function

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function fillShape(id){
         currentShape = `cross`;
     }
         fields[id] = currentShape;
-        console.log(fields)
+        console.log(fields);
+        draw();
+}
+
+function draw(){
+    for (let i = 0; i < fields.length; i++){
+        if (fields[i] == `circle`){
+            document.getElementById(`circle-${i}`).classList.remove(`d-none`)
+        }
+        if (fields[i] == `cross`){
+            document.getElementById(`cross-${i}`).classList.remove(`d-none`)
+        }
+    }
 }
